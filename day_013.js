f76d59a23829a4022cd158c093a2960d7810fe02
Yogendra Baskota <pas077bei045@wrc.edu.np>
2024-01-15 14:41:45 +0545

    .

---
+++ day_013.js
@@ -0,0 +1,30 @@
+// strings in javascript
+// strings = collection of charecters
+// strings are used to create and manupulate text.
+let namee = "johns"
+console.log(namee);
+console.log(namee.length);  // prints name of strings
+
+let friend = 'rajendra'
+console.log(friend);
+console.log(friend[0]) // print first character
+
+// template literals
+// Template literals use backtics instead of quotes to define a string
+// we can insert variables directly in template literal. This is called string interpolation.
+
+
+let boy1 = "pramod";
+let boy2 = "neil"
+
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence)
+
+// Escape sequence Characters
+let fruit = '\'Banana\''
+console.log(fruit);
+let a = "\"apple\""
+console.log(a)
+// \n = new line
+// \t = tab
+// \r = carriage return
