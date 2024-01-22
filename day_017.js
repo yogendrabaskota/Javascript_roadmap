6e7e4352fc06a46689cbfd5855516ff6bc71eaab
Yogendra Baskota <pas077bei045@wrc.edu.np>
2024-01-22 18:50:58 +0545

    .

---
+++ day_017.js
@@ -0,0 +1,16 @@
+// javascript array  methods
+let n = [1,7,9]
+n.toString()     // 1,7,9
+console.log(n)
+
+let m = [7,9,13]
+m.join("")    // 7-9-13
+console.log(m)
+
+let o = [1,2,4]
+o.pop()        // updates the original array and returns the popped value
+console.log(o)
+
+let a = [7,1,2,8]
+a.push(9)    // modifies the original array and returns the new array length
+console.log(a)
